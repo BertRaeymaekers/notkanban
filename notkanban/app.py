0b5513cd32790015e56743058c2ac30a9ec16d64
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2019-02-05 15:27:09 +0100

    First test Android app with beeware.

---
+++ notkanban/app.py
@@ -0,0 +1,11 @@
+from android import PythonActivity
+
+
+class MyApplication:
+        def onStart(self):
+                    print("Application starting up")
+
+
+                    app = MyApplication()
+
+                    activity = PythonActivity.setListener(app)
